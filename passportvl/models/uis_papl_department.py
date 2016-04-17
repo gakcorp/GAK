7a6dece13f6c57bae9c77fe1ecac5196d107f359
alexandrkopylov <5186441@gmail.com>
2016-04-17 23:41:11 +0300

    changes into passport+

--- passportvl/models/uis_papl_department.py
+++ passportvl/models/uis_papl_department.py
@@ -6,15 +6,46 @@ class uis_papl_department(models.Model):
 	_name='uis.papl.department'
 	_description='Organisation structure for Department'
 	name=fields.Char(string='Name')
-	substation_ids=fields.One2many('uis.papl.substation','department_id', string='Department')
-	substation_count=fields.Integer(string="Substations count", compute='_get_ss_calc')
-	apl_count=fields.Integer(string="APL count", compute='_get_ss_calc')
-	pillar_count=fields.Integer(string="Pillar count", compute='_get_ss_calc')
-	line_len=fields.Float(digits=(6,2), compute='_get_ss_calc')
+	control_summ=fields.Float(digits=(6,2),store=False, compute='_get_control_summ')
+	substation_ids=fields.One2many('uis.papl.substation','department_id', string='Substations')
+	substation_count=fields.Integer(string="Substations count", store=True, compute='_get_ss_calc')
+	apl_count=fields.Integer(string="APL count", store=True, compute='_get_ss_calc')
+	pillar_count=fields.Integer(string="Pillar count", store=True, compute='_get_ss_calc')
+	line_len=fields.Float(digits=(6,2), store=True, compute='_get_ss_calc')
 	parent_department_id=fields.Many2one('uis.papl.department', string='Parent Department')
 	url_maps=fields.Char(compute='_ss_get_url_maps', name='URL maps')
+	address=fields.Char(string="Address")
+	telephone=fields.Char(string="Phone num")
 	
-	@api.depends('substation_ids')
+	@api.onchange('control_summ')
+	def onchange_control_summ(self):
+		for depart in self:
+			depart._get_ss_calc
+			
+	@api.multi
+	def _get_control_summ(self):
+		for depart in self:
+			cs=0
+			for substation in depart.substation_ids:
+				cs=cs+1
+				for apl in substation.apl_id:
+					cs=cs+1
+					for pillar in apl.pillar_id:
+						cs=cs+1
+					cs=cs+apl.line_len_calc
+			child_depart_ids=self.search([('parent_department_id', '=', depart.id)])
+			for cdepart in child_depart_ids:
+				for substation in cdepart.substation_ids:
+					cs=cs+1
+					for apl in substation.apl_id:
+						cs=cs+1
+						for pillar in apl.pillar_id:
+							cs=cs+1
+						cs=cs+apl.line_len_calc	
+			#print depart
+			depart.control_summ=cs
+		
+	@api.depends('control_summ','substation_ids')
 	def _get_ss_calc(self):
 		for depart in self:
 			ssc=0
@@ -37,11 +68,13 @@ class uis_papl_department(models.Model):
 						for pillar in apl.pillar_id:
 							pillarc=pillarc+1
 						linelen=linelen+apl.line_len_calc	
+			#print depart
 			depart.apl_count=aplc
 			depart.substation_count=ssc
 			depart.pillar_count=pillarc
 			depart.line_len=linelen
-				
+			depart.control_summ=aplc+ssc+pillarc+linelen
+			
 	@api.depends('substation_ids')
 	def _ss_get_url_maps(self):
 		for depart in self:
@@ -57,7 +90,6 @@ class uis_papl_department(models.Model):
 			depart.url_maps=url
 	@api.multi
 	def act_show_map(self):
-		print "Debug info. Start Show_map for Department"
 		print self.url_maps
 		return{
 			'name': 'Maps',
