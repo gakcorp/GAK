dcc07b807687d60a5de8942d025b619028c657ac
alexandrkopylov <5186441@gmail.com>
2016-04-19 00:33:30 +0300

    menu changed and transformer module

--- passportvl/models/uis_papl_transformation.py
+++ passportvl/models/uis_papl_transformation.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 import math, urllib, json, time
 from openerp import models, fields, api
+class uis_papl_transformationa(models.Model):
+	_name='uis.papl.transformer'
+	_description='Transformer'
+	name=fields.Char(string='Name')s
