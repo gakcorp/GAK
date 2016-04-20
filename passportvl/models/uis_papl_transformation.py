23f7dad7af75b97a11a9f9fa51683720ce85a11c
alexandrkopylov <5186441@gmail.com>
2016-04-20 20:04:17 +0300

    changes in transformer

--- passportvl/models/uis_papl_transformation.py
+++ passportvl/models/uis_papl_transformation.py
@@ -7,17 +7,28 @@ class uis_papl_transformation(models.Model):
 	name=fields.Char(string='Name')
 	apl_id=fields.Many2one('uis.papl.apl', string='APL Name')
 	tap_id=fields.Many2one('uis.papl.tap', string='Tap Name')
-	pillar_id=fields.Many2one('uis.papl.pillar',string='Pillar Name', domain="[('id','in',near_pillar_ids)]")
+	pillar_id=fields.Many2one('uis.papl.pillar',string='Pillar Name', domain="[('id','in',near_pillar_ids.id)]")
 	longitude=fields.Float(digits=(2,6), string='Longitude')
 	latitude=fields.Float(digits=(2,6), string='Latitude')
-	near_pillar_ids=fields.Many2one('uis.papl.pillar', string='Near pillars ids', compute='_get_near_pillar_ids')
+	#near_pillar_ids=fields.Many2one('uis.papl.pillar', string='Near pillars ids', compute='_get_near_pillar')
+	near_pillar_ids=fields.Many2many('uis.papl.pillar',
+									 relation='near_pillar_ids',
+									 column1='trans_id',
+									 column2='pillar_id',
+									 compute='_get_near_pillar'
+									 )
+	#near_pillar_ids=fields.function(_get_near_pillar_ids,type='many2one',obj="uis.papl.pillar",method=True,string='Near pillars id'),
 	
-	def _get_near_pillar_ids(self,cr,uid,ids,context=None):
+	@api.depends('latitude','longitude')
+	def _get_near_pillar(self,cr,uid,ids,context=None):
 		for trans in self.browse(cr,uid,ids,context=context):
-			pillars = self.pool.get('uis.papl.pillar').search(cr,uid,[],context=context)
-			near_pillars=[]
 			lat1=trans.latitude
 			long1=trans.longitude
+			delta=0.01
+			max_dist=200
+			pillars = self.pool.get('uis.papl.pillar').search(cr,uid,[('latitude','>',lat1-delta),('latitude','<',lat1+delta),('longitude','>',long1-delta),('longitude','<',long1+delta)],context=context)
+			near_pillars=[]
+			near_pillars_ids=[]
 			for pid in pillars:
 				pillar=self.pool.get('uis.papl.pillar').browse(cr,uid,[pid],context=context)
 				if pillar:
@@ -44,12 +55,20 @@ class uis_papl_transformation(models.Model):
 						x = sl1*sl2+cl1*cl2*cdelta
 						ad = math.atan2(y,x)
 						dist = ad*rad
-					if (dist<200) and (dist>0):
-						near_pillars.append(pillar.id)
+					print str(pillar.id)+'='+str(dist)+' deltalat='+str(lat2-lat1)+' deltalong='+str(long2-long1)
+					if (dist<max_dist) and (dist>0):
+						print 'Distance to'+str(pillar.id)+'='+str(dist)+' < max_dist='+str(max_dist)
+						near_pillars.append(pillar)
+						near_pillars_ids.append(pillar.id)
 			#print near_pillars
-			trans.near_pillar_ids=near_pillars
-			print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-			print trans.near_pillar_ids
+			#trans.near_pillar_ids=near_pillars
+			#print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+			#print trans.near_pillar_ids
+			#trans.near_pillar_ids=pillar
+			#trans.near_pillar_ids=(5)
+			trans.near_pillar_ids=(6,False,near_pillars)
+			#print [(6,0,near_pillars_ids)]
+			#print trans.near_pillar_ids[1]
 
 		'''
 		
