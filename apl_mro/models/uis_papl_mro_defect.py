620e3a814e6a0dd2fc7756b35a0aba04710cdaa8
alexandrkopylov <5186441@gmail.com>
2016-05-12 00:05:37 +0300

    changes in mro_defect

--- apl_mro/models/uis_papl_mro_defect.py
+++ apl_mro/models/uis_papl_mro_defect.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import time
 
 from openerp.osv import fields, osv
@@ -5,6 +6,15 @@ import openerp.addons.decimal_precision as dp
 from openerp.tools.translate import _
 from openerp import netsvc
 
+'''
+class apl_mro_defect_type(osv.osv):
+    _name='uis.papl.defect.type'
+    _description='Transformer Types'
+    name=fields.Char(string='Name')
+    f_apl=fields.Boolean(string='APL')
+    f_pillar=fields.Boolean(string="Pillar")
+    f_trans=fields.Boolean(string="Pillar")
+  '''  
 class apl_mro_defect(osv.osv):
     """
     Defects
@@ -22,22 +32,22 @@ class apl_mro_defect(osv.osv):
     ]
 
     DEFECT_CATEGORY = [
-        ('0', 'Not Critical'),
-        ('1', 'Level 1'),
-        ('2', 'Level 2'),
-        ('3', 'Level 3'),
-        ('4', 'Level 4'),
-        ('5','Critical')
+        ('0', 'No Category'),
+        ('1', 'Critical'),
+        ('2', 'Normal'),
+        ('3', 'Not critical')
     ]
   
     _columns = {
-        'name': fields.char('Name of defects', size=64),
-        'apl_id': fields.many2one('uis.papl.apl', 'Air power line', required=False, readonly=True, states={'draft': [('readonly', False)]}),
+        'name': fields.char('Defect name', size=64),
+        #'defect_type':fields.many2one('uis.papl.mro.defect.type','Defect type',required=False, readonly=True, states={'draft': [('readonly', False)]}),
+        'apl_id': fields.many2one('uis.papl.apl', 'Air power line', readonly=True, required=False, states={'draft': [('readonly', False)]}),
         'tap_id': fields.many2one('uis.papl.tap', 'Tap of APL', required=False, readonly=True, states={'draft':[('readonly',False)]}),
         'pillar_id': fields.many2one('uis.papl.pillar', 'Pillar',required=False, readonly=True, states={'draft':[('readonly',False)]}),
         'transformer_id': fields.many2one('uis.papl.transformer','Transformer',required=False, readonly=True, states={'draft':[('readonly',False)]}),
         'unicode': fields.char('UniCode'),
         'state': fields.selection(STATE_SELECTION, 'Status', readonly=True),
+        'qnty':fields.integer('Qnt'),
         'labor_cost':fields.integer('Labor Cost'),
         'defect_description': fields.text('Defect Description'),
     }
