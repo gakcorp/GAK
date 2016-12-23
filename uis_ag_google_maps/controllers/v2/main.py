fd4e308e543afa793ab69d17f9620bc5f1fd1d41
alexandrkopylov <5186441@gmail.com>
2016-12-23 14:52:27 +0300

    Add photos to new lib

--- uis_ag_google_maps/controllers/v2/main.py
+++ uis_ag_google_maps/controllers/v2/main.py
@@ -63,7 +63,42 @@ class cartography_portal(http.Controller):
 					'longitude':ps.longitude,
 					'mark':mark
 				})
+		tlr.fix_end()
 		return ps_data
+	#NUPD Вынести в модуль аэрофотосъемки
+	def get_ph_list(self,mark_ids=None, apl_ids=None, ps_ids=None):
+		tlr=_ulog(code="MP_GEN_PHTLST",lib=__name__,desc="Generate photo list")
+		cr, uid,context=request.cr,request.uid,request.context
+		photo_data={"counter":0,"phs":[]}
+		photo_obj=request.registry['uis.ap.photo']
+		domain=[]
+		p_id=[]
+		photo_ids=photo_obj.search(cr, uid, domain, context=context)
+		for ph in photo_obj.browse(cr,uid,photo_ids,context=context).sorted(key=lambda r:r.image_date):
+			photo_data["counter"]+=1
+			_logger.debug(ph)
+			pill_data={
+				"counter":0,
+				"pillars":[]
+			}
+			for pil in ph.near_pillar_ids:
+				pill_data["counter"]=pill_data["counter"]+1
+				pill_data["pillars"].append({
+						'id':pil.id,
+						'num_by_vl':pil.num_by_vl
+						})
+			photo_data["phs"].append({
+				'id':ph.id,
+				'lat':ph.latitude,
+				'long':ph.longitude,
+				'alt':ph.altitude,
+				'thumbnail':'/web/image?model=uis.ap.photo&id='+str(ph.id)+'&field=thumbnail',
+				'url_image':'/web/image?model=uis.ap.photo&id='+str(ph.id)+'&field=image',
+				'rotation':ph.rotation,
+				'pillar_data':pill_data
+			})
+		tlr.fix_end()
+		return photo_data
 	@http.route('/maps', auth='public')
 	def maps(self, *arg, **post):
 		request.uid = request.session.uid
@@ -76,8 +111,10 @@ class cartography_portal(http.Controller):
 				pass
 		lr_data=self.get_layers_list()
 		ps_data=self.get_ps_list(ps_ids)
+		ph_data=self.get_ph_list()
 		values = {
 			'lr_data':lr_data,
-			'ps_data':ps_data
+			'ps_data':ps_data,
+			'ph_data':ph_data
 		}
 		return request.render("uis_ag_google_maps.uis_cartography_main", values)
--- uis_ag_google_maps/static/v2/js/custom.js
+++ uis_ag_google_maps/static/v2/js/custom.js
@@ -100,7 +100,30 @@ $(document).ready(function() {
             });
         }
     });
+    $SIDEBAR_PHOTO.find('a').on('click', function(ev) {
+        var $li = $(this).parent();
 
+        if ($li.is('.active')) {
+            $li.removeClass('active active-sm');
+            $('ul:first', $li).slideUp(function() {
+                setContentHeight();
+            });
+        } else {
+            // prevent closing menu if we are on child menu
+            if (!$li.parent().is('.child_menu')) {
+                $SIDEBAR_PHOTO.find('li').removeClass('active active-sm');
+                $SIDEBAR_PHOTO.find('li ul').slideUp();
+            }
+            
+            $li.addClass('active');
+
+            $('ul:first', $li).slideDown(function() {
+                setContentHeight();
+            });
+        }
+    });
+
+    
     // toggle small or large menu
     $MENU_TOGGLE.on('click', function() {
         if ($BODY.hasClass('nav-md')) {
