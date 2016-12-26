e8de5d7f5765ab1cac7c8d15a12a625bc0291b3c
alexandrkopylov <5186441@gmail.com>
2016-12-26 17:35:52 +0300

    Work with carto

--- uis_ag_google_maps/controllers/v2/main.py
+++ uis_ag_google_maps/controllers/v2/main.py
@@ -42,6 +42,7 @@ class cartography_portal(http.Controller):
 		cr, uid,context=request.cr,request.uid,request.context
 		ps_data={
 			"counter":0,
+			"counter_filtered":0,
 			"pss":[]
 		}
 		ps_obj=request.registry['uis.papl.substation']
@@ -51,8 +52,11 @@ class cartography_portal(http.Controller):
 			tlr.add_comment('[%r] power substation %r'%(ps.id,ps.name))
 			ps_data["counter"]+=1
 			mark=False
+			show=False
 			if ps.id in mark_ids:
 				mark=True
+				show=True
+				ps_data["counter_filtered"]+=1
 			ps_data["pss"].append({
 					'id':ps.id,
 					'name':ps.name,
@@ -61,10 +65,86 @@ class cartography_portal(http.Controller):
 					'department':ps.department_id.name,
 					'latitude':ps.latitude,
 					'longitude':ps.longitude,
-					'mark':mark
+					'mark':mark,
+					'show':show
 				})
 		tlr.fix_end()
 		return ps_data
+	def get_apl_list(self,filter_ids=None):
+		tlr=_ulog(code="MP_GEN_PSLST",lib=__name__,desc='Generate APL list')
+		cr, uid,context=request.cr,request.uid,request.context
+		apl_data={
+			"counter":0,
+			"counter_filtered":0,
+			"total_length":0,
+			"apls":[]
+		}
+		apl_obj=request.registry['uis.papl.apl']
+		domain=[]
+		apl_ids=apl_obj.search(cr,uid,domain,context=context)
+		for apl in apl_obj.browse(cr,uid,apl_ids,context=context).sorted(key=lambda r:r.name):
+			tlr.add_comment('[%r] apl %r'%(apl.id,apl.name))
+			apl_data["counter"]+=1
+			mark, show= False,False
+			if apl.id in filter_ids:
+				mark=True
+				show=True
+				apl_data["counter_filtered"]+=1
+			apl_data["total_length"]+=apl.line_len_calc
+			apl_data["apls"].append({
+				'id':apl.id,
+				'name':apl.name,
+				'mark':mark,
+				'show':show
+			})
+		tlr.fix_end()
+		return apl_data
+	def get_trans_list(self,filter_ids=None):
+		tlr=_ulog(code="MP_GEN_TRLST",lib=__name__,desc='Generate Trans List')
+		cr, uid,context=request.cr,request.uid,request.context
+		trans_data={
+			"counter":0,
+			"counter_filtered":0,
+			"transs":[]
+		}
+		trans_obj=request.registry['uis.papl.transformer']
+		domain=[]
+		trans_ids=trans_obj.search(cr,uid,domain,context=context)
+		for trans in trans_obj.browse(cr,uid,trans_ids, context=context).sorted(key=lambda r:r.name):
+			tlr.add_comment('[%r] Transformer %r'%(trans.id,trans.name))
+			trans_data["counter"]+=1
+			mark,show=False,False
+			if trans.id in filter_ids:
+				mark=True
+				show=True
+				trans_data["counter_filtered"]+=1
+			trans_data["transs"].append({
+				'id':trans.id,
+				'name':trans.name,
+				'mark':mark,
+				
+				})
+		tlr.fix_end()
+		return trans_data
+	def get_pillar_list(self,filter_ids=None):
+		tlr=_ulog(code="MP_GEN_PLRLST", lib=__name__,desc='Generate Pillar list')
+		cr,uid,context=request.cr,request.uid,request.context
+		pillar_data={
+			"counter":0,
+			"counter_filtered":0,
+			"pillars":[]
+		}
+		pillar_obj=request.registry['uis.papl.pillar']
+		domain=[]
+		pillar_ids=pillar_obj.search(cr,uid,domain, context=context)
+		for pillar in pillar_obj.browse(cr,uid,pillar_ids,context=context).sorted(key=lambda r:r.apl_id):
+			pillar_data["counter"]+=1
+			pillar_data["pillars"].append({
+				'id':pillar.id,
+				'name':pillar.name
+				})
+		tlr.fix_end()
+		return pillar_data
 	#NUPD Вынести в модуль аэрофотосъемки
 	def get_ph_list(self,mark_ids=None, apl_ids=None, ps_ids=None):
 		tlr=_ulog(code="MP_GEN_PHTLST",lib=__name__,desc="Generate photo list")
@@ -104,17 +184,31 @@ class cartography_portal(http.Controller):
 		request.uid = request.session.uid
 		cr,uid,context=request.cr,request.uid,request.context
 		ps_ids=[]
+		apl_ids=[]
+		trans_ids=[]
 		for ps_id in post.get('ps_ids',"").split(","):
 			try:
 				ps_ids.append(int(ps_id))
 			except ValueError:
 				pass
+		for apl_id in post.get('apl_ids',"").split(","):
+			try:
+				apl_ids.append(int(apl_id))
+			except ValueError:
+				pass
+		
 		lr_data=self.get_layers_list()
 		ps_data=self.get_ps_list(ps_ids)
+		apl_data=self.get_apl_list(apl_ids)
+		trans_data=self.get_trans_list(trans_ids)
 		ph_data=self.get_ph_list()
+		pillar_data=self.get_pillar_list()
 		values = {
 			'lr_data':lr_data,
 			'ps_data':ps_data,
+			'apl_data':apl_data,
+			'trans_data':trans_data,
+			'pillar_data':pillar_data,
 			'ph_data':ph_data
 		}
 		return request.render("uis_ag_google_maps.uis_cartography_main", values)
