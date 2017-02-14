c0f2177175004fc6424264a38a996a292c8c49a4
alexandrkopylov <5186441@gmail.com>
2017-02-15 00:31:04 +0300

    st1 p10
    changes in _apl_photo_count function and photo_ids relation

--- apl_aerial_photo/models/mod_uis_papl_apl.py
+++ apl_aerial_photo/models/mod_uis_papl_apl.py
@@ -9,7 +9,7 @@ class uis_apl_aerial_photo_mod_uis_papl_apl(osv.Model):
 	_name = 'uis.papl.apl'
 	_inherit = 'uis.papl.apl'
 
-	def _apl_photo_count(self, cr, uid, ids, field_name, arg, context=None):
+	def _apl_photo_count_old(self, cr, uid, ids, field_name, arg, context=None):
 		res = dict.fromkeys(ids, 0)
 		photo = self.pool['uis.ap.photo']
 		_logger.debug('Request for count of photo for APL %r'%ids)
@@ -20,14 +20,22 @@ class uis_apl_aerial_photo_mod_uis_papl_apl(osv.Model):
 			pc=len(pids)
 			res[apl.id] = pc
 		return res
-	
+	def _apl_photo_count(self,cr,uid,ids,field_name, arg, context=None):
+		res=dict.fromkeys(ids,0)
+		for apl in self.browse(cr,uid,ids,context=context):
+			_logger.debug ('Photo for apl id[%r] is %r'%(apl.id,apl.photo_ids))
+			for ph in apl.photo_ids:
+				_logger.debug('[_apl_photo_count] debug ingormation = %r'%ph.id)
+			photo_count=len(apl.photo_ids)
+			res[apl.id] = photo_count
+		return res
 	def _get_photo_ids(self, cr, uid, ids, field_name, arg, context=None):
 		res=dict.fromkeys(ids,0)
 		return res
 		
 	_columns = {
 		'photo_count': fields.function(_apl_photo_count, string='# Photo', type='integer'),
-		'photo_ids':fields.many2many('uis.ap.photo',rel='photo_near_apl', id1='apl_id', id2='photo_id',string="Photos")
+		'photo_ids':fields.many2many('uis.ap.photo',rel='photo_apl_rel', id1='apl_id', id2='photo_id',string="Photos")
 	}
 
 	def action_view_photos(self, cr, uid, ids, context=None):
