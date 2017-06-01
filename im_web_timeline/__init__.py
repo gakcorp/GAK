0ffbaaef7cc68315b6fd0f602663166638cabd33
AK <a.kopylov@inlinegroup.ru>
2017-06-01 19:08:10 +0300

    add draft timeline widget

---
+++ im_web_timeline/__init__.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+############################################################################
+#
+# Intellium Inc, web timeline widget
+# Copyright (C) 2017
+#
+# @author Alexandr Kopylov<etienne.b.auger@savoirfairelinux.com>
+#
+##############################################################################
